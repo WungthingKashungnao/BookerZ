753fdc070ffbce3efbec61ba3dd38c94ec4cc5a0
wungthing <wungthingkashungnao123@gmail.com>
2023-04-10 15:32:36 +0530

    added room reservation funcionaliy

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,29 +1,38 @@
-
-import express from 'express'
-import { createRoom, deleteRoom, getRoom, getallRoom, updateRoom } from '../controllers/roomController.js';
-import { verifyAdmin } from '../utils/verifyToken.js';
-
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getallRoom,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // create start
-router.post('/:hotelId', verifyAdmin, createRoom)
+router.post("/:hotelId", verifyAdmin, createRoom);
 // create end
 
 // update start
-router.patch('/:id', verifyAdmin, updateRoom)
+router.patch("/:id", verifyAdmin, updateRoom);
 // update end
 
 // delete start
-router.delete('/:id/:hotelId', verifyAdmin, deleteRoom)
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
 // delete end
 
 // get start
-router.get('/:id', getRoom)
+router.get("/:id", getRoom);
 // get end
 
 // get all start
-router.get('/', getallRoom)
+router.get("/", getallRoom);
 // get all end
 
-export default router
+// update room availabilliy room start
+router.patch("/availability/:id", updateRoomAvailability);
+// update room availabilliy room end
+
+export default router;
